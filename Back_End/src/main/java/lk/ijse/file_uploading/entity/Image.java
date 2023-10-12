e0ef8c82b08913e04a48d5dd0479dc6fb569cb12
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-13 01:17:52 +0530

    dto entity class added

---
+++ Back_End/src/main/java/lk/ijse/file_uploading/entity/Image.java
@@ -0,0 +1,28 @@
+package lk.ijse.file_uploading.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Integer id;
+    private String fileString;
+
+    public Image(String fileString) {
+        this.fileString = fileString;
+    }
+}
