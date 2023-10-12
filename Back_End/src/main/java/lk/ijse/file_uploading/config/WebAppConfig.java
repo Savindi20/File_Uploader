5b03337e9c51c7a9dbb871b5f36b81461a0b97cd
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-12 22:51:17 +0530

    web app config updated

--- Back_End/src/main/java/lk/ijse/file_uploading/config/WebAppConfig.java
+++ Back_End/src/main/java/lk/ijse/file_uploading/config/WebAppConfig.java
@@ -1,14 +1,27 @@
 package lk.ijse.file_uploading.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"lk.ijse.file_uploading.config"})
-public class WebAppConfig {
-    public WebAppConfig(){
-        System.out.println("WebAppConfig: Instantiated");
+@ComponentScan(basePackages = {"lk.ijse.file_uploading"})
+public class WebAppConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**").addResourceLocations("/uploads/");
+    }
+
 }
