ee5a81cef19e2d72d1b42c565625809060c269b9
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-13 01:27:24 +0530

    exception handler updated

--- Back_End/src/main/java/lk/ijse/file_uploading/advisor/AppWideExceptionHandler.java
+++ Back_End/src/main/java/lk/ijse/file_uploading/advisor/AppWideExceptionHandler.java
@@ -1,20 +1,18 @@
 package lk.ijse.file_uploading.advisor;
 
 
-import lk.ijse.file_uploading.util.ResponseUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
 @RestControllerAdvice
 @CrossOrigin
 public class AppWideExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler({RuntimeException.class})
-    public ResponseUtil handleMyExceptions(RuntimeException e) {
-        return new ResponseUtil("Error", e.getMessage(), null);
+    public void handleMyExceptions(RuntimeException e) {
+        System.out.println(e.getMessage());
     }
 }
