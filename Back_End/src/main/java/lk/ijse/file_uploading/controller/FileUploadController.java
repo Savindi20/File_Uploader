2fcf712504deca57cc492d91bdbd7027eb57df83
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-13 01:47:53 +0530

    service files updated

--- Back_End/src/main/java/lk/ijse/file_uploading/controller/FileUploadController.java
+++ Back_End/src/main/java/lk/ijse/file_uploading/controller/FileUploadController.java
@@ -1,6 +1,12 @@
 package lk.ijse.file_uploading.controller;
 
+import lk.ijse.file_uploading.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import lk.ijse.file_uploading.dto.ImageDTO;
+import lk.ijse.file_uploading.entity.Image;
+import lk.ijse.file_uploading.repo.FileUploadRepo;
 import lk.ijse.file_uploading.service.FileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
@@ -20,37 +26,35 @@ import java.util.Base64;
 @RequestMapping("upload")
 @CrossOrigin
 public class FileUploadController {
-    @Autowired
-    private FileUploadService fileRepo;
+        @Autowired
+        private FileUploadService fileRepo;
 
-    @PostMapping
-    public String uploadFileWithSpringWay(@RequestPart("image") MultipartFile myFile, ModelMap modelMap) {
-        modelMap.addAttribute("file", myFile);
-        try {
-            byte[] byteArray = myFile.getBytes();
-//          String projectPath = "D:\\Project\\File_Upload_with_Spring-MVC\\Front_End\\assets\\image";
-            String projectPath = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile().getParentFile().getAbsolutePath();
-            String path = "/" + myFile.getOriginalFilename();
-            Path location = Paths.get(projectPath + path);
-            Files.write(location, byteArray);
-            myFile.transferTo(location);
-            System.out.println(projectPath);
+        @PostMapping
+        public String uploadFileWithSpringWay(@RequestPart("image") MultipartFile myFile, ModelMap modelMap) {
+            modelMap.addAttribute("file", myFile);
+            try {
+                byte[] byteArray = myFile.getBytes();
+//            String projectPath = "D:\\Project\\File_Upload_with_Spring-MVC\\Front_End\\assets\\image";
+                String projectPath = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile().getParentFile().getAbsolutePath();
+                String path = "/" + myFile.getOriginalFilename();
+                Path location = Paths.get(projectPath + path);
+                Files.write(location, byteArray);
+                myFile.transferTo(location);
+                System.out.println(projectPath);
 
-//           String location2 = "assets/" + myFile.getOriginalFilename();
-            fileRepo.saveFileLocation(new ImageDTO(null, location.toString()));
-            return Base64.getEncoder().encodeToString(byteArray);
+//            String location2 = "assets/" + myFile.getOriginalFilename();
+                fileRepo.saveFileLocation(new ImageDTO(null, location.toString()));
+                return Base64.getEncoder().encodeToString(byteArray);
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
         }
-    }
+            @GetMapping
+            public String getAll() {
+                return fileRepo.getLastImageLocation();
+            }
 
-
-    @GetMapping
-    public ArrayList<ImageDTO> getAll() {
-        return fileRepo.loadFileLocation();
-    }
-
-}
+        }
--- Back_End/src/main/java/lk/ijse/file_uploading/service/FileUploadService.java
+++ Back_End/src/main/java/lk/ijse/file_uploading/service/FileUploadService.java
@@ -1,12 +1,9 @@
 package lk.ijse.file_uploading.service;
 
 import lk.ijse.file_uploading.dto.ImageDTO;
-import lk.ijse.file_uploading.dto.ImageDTO;
 import org.springframework.web.bind.annotation.ModelAttribute;
-
-import java.util.ArrayList;
 
 public interface FileUploadService {
     void saveFileLocation(@ModelAttribute ImageDTO dto);
-    ArrayList<ImageDTO> loadFileLocation();
+    String getLastImageLocation();
 }
