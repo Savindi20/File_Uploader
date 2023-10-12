b921aca5f2e5a15f4ab499d14f3f1fbb016f3311
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-12 22:28:36 +0530

    alet.js added and file uploaderjs file updated"

---
+++ Back_End/src/main/java/lk/ijse/file_uploading/controller/FileUploadController.java
@@ -0,0 +1,10 @@
+package lk.ijse.file_uploading.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("upload")
+@CrossOrigin
+public class FileUploadController {
+
+}
