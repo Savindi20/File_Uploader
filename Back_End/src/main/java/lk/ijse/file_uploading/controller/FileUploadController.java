b7ef35f0e0792f9c3e7be891c489ed6399deaec2
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-13 00:58:17 +0530

    controller file updated

--- Back_End/src/main/java/lk/ijse/file_uploading/controller/FileUploadController.java
+++ Back_End/src/main/java/lk/ijse/file_uploading/controller/FileUploadController.java
@@ -1,11 +1,9 @@
 package lk.ijse.file_uploading.controller;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -15,9 +13,7 @@ import java.util.ArrayList;
 @RequestMapping("upload")
 @CrossOrigin
 public class FileUploadController {
-
     private static final ArrayList<String> allImages = new ArrayList<>();
-
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity uploadFileWithSpringWay(@RequestPart("image") MultipartFile myFile) {
         try {
@@ -25,16 +21,13 @@ public class FileUploadController {
             File uploadsDir = new File(projectPath + "/uploads");
             uploadsDir.mkdir();
             myFile.transferTo(new File(uploadsDir.getAbsolutePath() + "/" + myFile.getOriginalFilename()));
-
             allImages.add("uploads/" + myFile.getOriginalFilename());
-
             return ResponseEntity.ok(HttpStatus.OK);
         } catch (URISyntaxException | IOException e) {
             e.printStackTrace();
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ArrayList<String>> getAllImagesFromDatabase() {
         return new ResponseEntity<>(allImages, HttpStatus.OK);
