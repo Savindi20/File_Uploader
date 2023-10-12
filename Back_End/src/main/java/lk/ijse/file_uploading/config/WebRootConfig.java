d8053773c6aedf006062414c19dd7fe8e5d75fcd
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-13 01:15:45 +0530

    webroot class updated

--- Back_End/src/main/java/lk/ijse/file_uploading/config/WebRootConfig.java
+++ Back_End/src/main/java/lk/ijse/file_uploading/config/WebRootConfig.java
@@ -1,10 +1,17 @@
 package lk.ijse.file_uploading.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(JPAConfig.class)
+@ComponentScan(basePackages = "lk.ijse.file_uploading.service")
 public class WebRootConfig {
-    public WebRootConfig(){
-        System.out.println("WebRootConfig: Instantiated");
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 }
