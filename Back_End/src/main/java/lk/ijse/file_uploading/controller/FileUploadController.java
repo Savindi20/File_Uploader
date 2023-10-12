bf8f7b44b2aa27f87a9cbb59866a828f6c1ba29d
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-10-13 01:55:12 +0530

    content updated

--- Back_End/src/main/java/lk/ijse/file_uploading/controller/FileUploadController.java
+++ Back_End/src/main/java/lk/ijse/file_uploading/controller/FileUploadController.java
@@ -1,9 +1,4 @@
 package lk.ijse.file_uploading.controller;
-
-import lk.ijse.file_uploading.util.ResponseUtil;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import lk.ijse.file_uploading.dto.ImageDTO;
 import lk.ijse.file_uploading.entity.Image;
 import lk.ijse.file_uploading.repo.FileUploadRepo;
@@ -12,11 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -26,35 +19,30 @@ import java.util.Base64;
 @RequestMapping("upload")
 @CrossOrigin
 public class FileUploadController {
-        @Autowired
-        private FileUploadService fileRepo;
-
-        @PostMapping
-        public String uploadFileWithSpringWay(@RequestPart("image") MultipartFile myFile, ModelMap modelMap) {
-            modelMap.addAttribute("file", myFile);
-            try {
-                byte[] byteArray = myFile.getBytes();
+    @Autowired
+    private FileUploadService fileRepo;
+    @PostMapping
+    public String uploadFileWithSpringWay(@RequestPart("image") MultipartFile myFile, ModelMap modelMap) {
+        modelMap.addAttribute("file", myFile);
+        try {
+            byte[] byteArray = myFile.getBytes();
 //            String projectPath = "D:\\Project\\File_Upload_with_Spring-MVC\\Front_End\\assets\\image";
-                String projectPath = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile().getParentFile().getAbsolutePath();
-                String path = "/" + myFile.getOriginalFilename();
-                Path location = Paths.get(projectPath + path);
-                Files.write(location, byteArray);
-                myFile.transferTo(location);
-                System.out.println(projectPath);
-
+            String projectPath = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile().getParentFile().getAbsolutePath();
+            String path = "/" + myFile.getOriginalFilename();
+            Path location = Paths.get(projectPath + path);
+            Files.write(location, byteArray);
+            myFile.transferTo(location);
+            System.out.println(projectPath);
 //            String location2 = "assets/" + myFile.getOriginalFilename();
-                fileRepo.saveFileLocation(new ImageDTO(null, location.toString()));
-                return Base64.getEncoder().encodeToString(byteArray);
+            fileRepo.saveFileLocation(new ImageDTO(null, location.toString()));
+            return Base64.getEncoder().encodeToString(byteArray);
 
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } catch (URISyntaxException e) {
-                throw new RuntimeException(e);
-            }
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
         }
-            @GetMapping
-            public String getAll() {
-                return fileRepo.getLastImageLocation();
-            }
-
-        }
+    }
+    @GetMapping
+    public String getAll() {
+        return fileRepo.getLastImageLocation();
+    }
+}
